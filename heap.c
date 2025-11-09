7b28f43fe22d88b2072427731e278099ba133fe7
SakshiBisht-16 <sakshiibishhtt@gmail.com>
2025-11-09 14:52:24 +0530

    Update heap.c

--- heap.c
+++ heap.c
@@ -1,41 +1,44 @@
 #include "heap.h"
+#include "bst.h"
 #include <stdlib.h>
 #include <string.h>
 
-#define TOTAL_MEMORY 256
+#define TOTAL_MEMORY 1024
 
 Block *head = NULL;
+BSTNode *free_tree = NULL;
 
 void heap_init(void) {
-    head = (Block*)malloc(sizeof(Block));
+    head = malloc(sizeof(Block));
     head->pid = -1;
     head->start = 0;
     head->size = TOTAL_MEMORY;
     head->free = 1;
     head->next = NULL;
+    free_tree = bst_insert(NULL, head);
 }
 
 Block* heap_allocate(int pid, size_t size) {
-    Block *curr = head;
-    while (curr) {
-        if (curr->free && curr->size >= size) {
-            if (curr->size > size) {
-                Block *new_blk = (Block*)malloc(sizeof(Block));
-                new_blk->pid = -1;
-                new_blk->start = curr->start + size;
-                new_blk->size = curr->size - size;
-                new_blk->free = 1;
-                new_blk->next = curr->next;
-                curr->next = new_blk;
-                curr->size = size;
-            }
-            curr->pid = pid;
-            curr->free = 0;
-            return curr;
-        }
-        curr = curr->next;
+    Block *best = bst_find_best_fit(free_tree, size);
+    if (!best) return NULL;
+
+    free_tree = bst_delete(free_tree, best->size);
+
+    if (best->size > size) {
+        Block *new_blk = malloc(sizeof(Block));
+        new_blk->pid = -1;
+        new_blk->start = best->start + size;
+        new_blk->size = best->size - size;
+        new_blk->free = 1;
+        new_blk->next = best->next;
+        best->next = new_blk;
+        best->size = size;
+        free_tree = bst_insert(free_tree, new_blk);
     }
-    return NULL;
+
+    best->pid = pid;
+    best->free = 0;
+    return best;
 }
 
 void heap_free(int pid) {
@@ -44,21 +47,10 @@ void heap_free(int pid) {
         if (curr->pid == pid) {
             curr->pid = -1;
             curr->free = 1;
+            free_tree = bst_insert(free_tree, curr);
             break;
         }
         curr = curr->next;
-    }
-    // merge adjacent free blocks
-    curr = head;
-    while (curr && curr->next) {
-        if (curr->free && curr->next->free) {
-            Block *tmp = curr->next;
-            curr->size += tmp->size;
-            curr->next = tmp->next;
-            free(tmp);
-        } else {
-            curr = curr->next;
-        }
     }
 }
 
@@ -72,7 +64,7 @@ void heap_compact(void) {
         }
         curr = curr->next;
     }
-    // move all free memory to the end
+
     Block *new_head = NULL, *tail = NULL;
     curr = head;
     while (curr) {
@@ -83,8 +75,9 @@ void heap_compact(void) {
         }
         curr = next;
     }
+
     if (current_start < TOTAL_MEMORY) {
-        Block *free_blk = (Block*)malloc(sizeof(Block));
+        Block *free_blk = malloc(sizeof(Block));
         free_blk->pid = -1;
         free_blk->start = current_start;
         free_blk->size = TOTAL_MEMORY - current_start;
@@ -92,6 +85,8 @@ void heap_compact(void) {
         free_blk->next = NULL;
         if (!new_head) new_head = free_blk;
         else { tail->next = free_blk; tail = free_blk; }
+        bst_clear(free_tree);
+        free_tree = bst_insert(NULL, free_blk);
     } else {
         tail->next = NULL;
     }
