b2bc9c16412b2302580a801bcbbca8c2b236214b
SakshiBisht-16 <sakshiibishhtt@gmail.com>
2025-11-11 12:05:42 +0530

    Update memory_manager.h

--- memory_manager.h
+++ memory_manager.h
@@ -4,41 +4,51 @@
 #include <stddef.h>
 
 #define TOTAL_MEMORY 1024
+#define MAX_PROCESSES 100
 
 typedef struct Block {
     int pid;
     size_t size;
     int allocated;
-    struct Block* next;
+    struct Block *next;
 } Block;
 
 typedef struct {
-    Block* head;
+    Block *head;
 } Heap;
 
-// Graph structures
 typedef struct Node {
     int pid;
-    struct Node* next;
+    struct Node *next;
 } Node;
 
 typedef struct {
-    Node* adjList[100];
+    Node *adjList[MAX_PROCESSES];
 } Graph;
 
 extern Heap heap;
 extern Graph proc_graph;
+extern int pid_counter;
 
-// Heap functions
+/* Memory functions */
 void heap_init(void);
 int memory_allocate_auto(size_t size);
 int memory_free_pid(int pid);
 void memory_compact(void);
 void display_memory(void);
 
-// Graph functions
-void graph_init(Graph* g);
-void graph_add_edge(Graph* g, int from, int to);
-void graph_display(Graph* g);
+/* Graph functions */
+void graph_init(Graph *g);
+void graph_add_edge(Graph *g, int from, int to);
+void graph_display(Graph *g);
+
+/* Persistence */
+void save_heap(const char *filename);
+void load_heap(const char *filename);
+void save_graph(const char *filename);
+void load_graph(const char *filename);
+
+/* Session reset */
+void reset_system(void);
 
 #endif
